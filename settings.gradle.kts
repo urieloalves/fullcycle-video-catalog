1f1c2a55066c49707c61a8c16f2f9b32179f673f
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-06-03 13:58:55 +0200

    create gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "fullcycle-video-catalog"
+
---
+++ src/main/java/dev/urieloalves/Main.java
@@ -0,0 +1,7 @@
+package dev.urieloalves;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
