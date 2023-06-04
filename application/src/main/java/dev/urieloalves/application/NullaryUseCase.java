ace658a24b9253e0e6fc0ac0424e191d9d461e48
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-06-04 12:30:56 +0200

    add use case helper classes

---
+++ application/src/main/java/dev/urieloalves/application/NullaryUseCase.java
@@ -0,0 +1,6 @@
+package dev.urieloalves.application;
+
+public abstract class NullaryUseCase<OUT> {
+
+    public abstract OUT execute();
+}
---
+++ application/src/main/java/dev/urieloalves/application/UnitUseCase.java
@@ -0,0 +1,6 @@
+package dev.urieloalves.application;
+
+public abstract class UnitUseCase<IN> {
+
+    public abstract void execute(IN in);
+}
