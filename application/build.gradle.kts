6b427dfcaaca6a7e05ad78e4b4eee2653efc5bd7
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2023-06-04 12:35:26 +0200

    install mockito

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
 }
 
 tasks.test {
---
+++ application/src/test/java/dev/urieloalves/application/UseCaseTest.java
@@ -0,0 +1,14 @@
+package dev.urieloalves.application;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UseCaseTest {
+
+    @Test
+    public void testCreateUseCase() {
+
+    }
+}
